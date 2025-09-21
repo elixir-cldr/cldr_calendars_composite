f1969ab829a5e5657387267672d67042191906c3
Kip Cole <kipcole9@gmail.com>
2025-09-21 21:27:07 +1000

    Add Russian calendar

--- mix/england.ex
+++ mix/england.ex
@@ -1,8 +1,17 @@
 defmodule Cldr.Calendar.England do
+  @moduledoc """
+  Composite English calendar.
+
+  See:
+
+    https://libguides.ctstatelibrary.org/hg/colonialresearch/calendar
+    https://en.wikipedia.org/wiki/Julian_calendar
+
+  """
   use Cldr.Calendar.Composite,
     calendars: [
       ~D[1155-03-25 Cldr.Calendar.Julian.March25],
-      ~D[1751-03-25 Cldr.Calendar.Julian],
+      ~D[1751-03-25 Cldr.Calendar.Julian.Jan1],
       ~D[1752-09-14 Cldr.Calendar.Gregorian]
       ],
     base_calendar: Cldr.Calendar.Julian
---
+++ mix/russia.ex
@@ -0,0 +1,20 @@
+defmodule Cldr.Calendar.Russia do
+  @moduledoc """
+  Composite Russian Calendar.
+
+  See:
+
+    https://www.justrussian.com/history-of-new-year-celebrations-in-russia/
+    https://en.wikipedia.org/wiki/Byzantine_calendar
+    https://en.wikipedia.org/wiki/Julian_calendar
+
+  """
+  use Cldr.Calendar.Composite,
+    calendars: [
+
+      ~D[1492-09-01 Cldr.Calendar.Julian.Sept1],
+      ~D[1700-01-01 Cldr.Calendar.Julian.Jan1],
+      ~D[1918-02-14 Cldr.Calendar.Gregorian]
+    ],
+    base_calendar: Cldr.Calendar.Julian.March1
+end
